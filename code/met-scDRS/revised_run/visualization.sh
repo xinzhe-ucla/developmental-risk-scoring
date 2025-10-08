4e56d9c98b1b8658c709caf382a8725305b264f0
Xinzhe Li <lixinzhe@ucla.edu>
2025-10-08 11:19:14 -0700

    compiled

--- code/met-scDRS/revised_run/visualization.sh
+++ code/met-scDRS/revised_run/visualization.sh
@@ -56,4 +56,39 @@ Rscript /u/home/l/lixinzhe/project-github/developmental-risk-scoring/code/met-sc
     --cutoff 0.1 \
     --out "/u/home/l/lixinzhe/project-geschwind/plot/revised-intial_run_developmental_methylation_umap/exc/"
 
-## 
+###########################################################################################
+######                     Visualize with covariates regressed                       ######
+###########################################################################################
+current_date=$(date +"%Y-%m-%d")
+
+Rscript /u/home/l/lixinzhe/project-github/developmental-risk-scoring/code/met-scDRS/revised_run/proportion-heatmap.R \
+    --dir '/u/home/l/lixinzhe/project-geschwind/result/met-scDRS/dev-revised/cov/' \
+    --meta_data "/u/project/cluo/heffel/BICAN3/DATA/metadata_09122025.csv.gz" \
+    --field 'newL1' \
+    --cutoff 0.1 \
+    --out "/u/home/l/lixinzhe/project-geschwind/plot/${current_date}-revised-BICAN-mcg-l1-proportion.png" \
+    --plot_type "proportion"
+
+Rscript /u/home/l/lixinzhe/project-github/developmental-risk-scoring/code/met-scDRS/revised_run/proportion-heatmap.R \
+    --dir '/u/home/l/lixinzhe/project-geschwind/result/met-scDRS/dev-revised/cov/' \
+    --meta_data "/u/project/cluo/heffel/BICAN3/DATA/metadata_09122025.csv.gz" \
+    --field 'newL2' \
+    --cutoff 0.1 \
+    --out "/u/home/l/lixinzhe/project-geschwind/plot/${current_date}-revised-BICAN-mcg-l2-proportion.png" \
+    --plot_type "proportion"
+
+Rscript /u/home/l/lixinzhe/project-github/developmental-risk-scoring/code/met-scDRS/revised_run/proportion-heatmap.R \
+    --dir '/u/home/l/lixinzhe/project-geschwind/result/met-scDRS/dev-revised/cov/' \
+    --meta_data "/u/project/cluo/heffel/BICAN3/DATA/metadata_09122025.csv.gz" \
+    --field 'newL3' \
+    --cutoff 0.1 \
+    --out "/u/home/l/lixinzhe/project-geschwind/plot/${current_date}-revised-BICAN-mcg-l3-proportion.png" \
+    --plot_type "proportion"
+
+Rscript /u/home/l/lixinzhe/project-github/developmental-risk-scoring/code/met-scDRS/revised_run/proportion-heatmap.R \
+    --dir '/u/home/l/lixinzhe/project-geschwind/result/met-scDRS/dev-revised/cov/' \
+    --meta_data "/u/project/cluo/heffel/BICAN3/DATA/metadata_09122025.csv.gz" \
+    --field 'fine_age_groups' \
+    --cutoff 0.1 \
+    --out "/u/home/l/lixinzhe/project-geschwind/plot/${current_date}-revised-BICAN-mcg-fine-age-proportion.png" \
+    --plot_type "proportion"
