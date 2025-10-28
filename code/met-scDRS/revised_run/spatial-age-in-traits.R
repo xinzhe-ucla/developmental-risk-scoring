5fcb5596d7199d48a91aa67824c83f309ad7f26f
Xinzhe Li <lixinzhe@ucla.edu>
2025-10-28 09:14:07 -0700

    compiled

--- code/met-scDRS/revised_run/spatial-age-in-traits.R
+++ code/met-scDRS/revised_run/spatial-age-in-traits.R
@@ -12,7 +12,7 @@ names(risk.score) <- score.files;
 
 mdd = data.frame(
     fread(
-        file =  "/u/home/l/lixinzhe/project-geschwind/result/met-scDRS/dev-revised//PASS_MDD_Howard2019.score.gz",
+        file =  "/u/home/l/lixinzhe/project-geschwind/result/met-scDRS/dev-revised/cov/PASS_MDD_Howard2019.score.gz",
         sep = '\t',
         header = TRUE,
         data.table = FALSE
@@ -175,6 +175,18 @@ sig_summary <- frame %>%
     .groups = "drop"
   )
 
+total_summary <- frame %>%
+  group_by(newL3, fine_age_groups) %>%
+  summarise(
+    n_total = n(),
+    .groups = "drop"
+  )
+  
+mat_total <- total_summary %>%
+  select(newL3, fine_age_groups, n_total) %>%
+  pivot_wider(names_from = fine_age_groups, values_from = n_total)
+
+
 # wide matrix: rows = newL3, cols = fine_age_groups, values = prop_sig
 mat_df <- sig_summary %>%
   select(newL3, fine_age_groups, prop_sig) %>%
@@ -184,6 +196,15 @@ mat_df <- sig_summary %>%
 rn <- mat_df$newL3
 mat <- as.matrix(mat_df[,-1])
 rownames(mat) <- rn
+
+total_mat <- as.matrix(mat_total[, -1])
+rn <- mat_total$newL3
+rownames(total_mat) <- rn
+
+stopifnot(rownames(mat) == rownames(total_mat))
+
+# if a matrix has less than 50 cells, color it grey:
+mat[total_mat < 50] = NA
 
 library(ComplexHeatmap)
 library(circlize)
@@ -242,166 +263,66 @@ draw(plot)
 dev.off();
 
 ###########################################################################################
-######                                    Specific UMAP                              ######
+######                                    Lineage plot                               ######
 ###########################################################################################
-# Chongyuan: Could you plot the following cells on UMAP for SCZ? 1m - L6b, MSN cells, and Cable 1. 7m - TCx? 
+lineage_step1 = rownames(meta)[meta$fine_age_groups == '2T' & meta$newL3 == 'RG-1']
+lineage_step2 = rownames(meta)[meta$fine_age_groups == '2T' & meta$newL3 == 'RG-UL']
+lineage_step3 = rownames(meta)[meta$fine_age_groups == '3T' & meta$newL3 == 'L4-RORB']
+lineage_step4 = rownames(meta)[meta$fine_age_groups == '1m' & meta$newL3 == 'L4-PLCH1']
+lineage_step5 = rownames(meta)[meta$fine_age_groups == '4m' & meta$newL3 == 'L4-PLCH1']
+lineage_step6 = rownames(meta)[meta$fine_age_groups == '7m' & meta$newL3 == 'L4-PLCH1']
+lineage_step7 = rownames(meta)[meta$fine_age_groups == 'adult' & meta$newL3 == 'L4-PLCH1']
 
-# 
-month_1 = rownames(meta)[meta$fine_age_groups == '1m']
-month_7 = rownames(meta)[meta$fine_age_groups == '7m']
-l6b = rownames(meta)[meta$newL3 == 'L6b']
-msn = rownames(meta)[meta$newL3 == 'MSN']
-cables1 = rownames(meta)[meta$newL3 == 'CABLES1']
-tcx = rownames(meta)[meta$newL3 == 'TCx']
+disease = 'PASS_Schizophrenia_Pardinas2018'
+score = risk.score[[disease]]
 
-cells_to_plot = c(
-    intersect(month_1, l6b),
-    intersect(month_1, msn),
-    intersect(month_1, cables1),
-    intersect(month_7, tcx)
-    )
+total_lineage = list(lineage_step1, lineage_step2, lineage_step3, lineage_step4, lineage_step5, lineage_step6, lineage_step7)
+names(total_lineage) = paste0('step', seq(1, length(total_lineage)))
 
-# read meta:
-umap <- read.csv(
-    header = TRUE,
-    row.names = 1,
-    file = '/u/home/l/lixinzhe/project-geschwind/port/scratch/met_scdrs_dev/joint_umap_coords_281146.csv'
-    );
+plot_dfs = NULL
+for (step in names(total_lineage)){
+    plot_dfs[[step]] = data.frame(
+        'score' = score[total_lineage[[step]], ],
+        'lineage_step' = step
+        )
+}
+plot_df = Reduce('rbind', plot_dfs)
 
-plot.df <- risk.score[[disease]];
-plot.df$umap1 <- umap[rownames(plot.df), 'X_umap'];
-plot.df$umap2 <- umap[rownames(plot.df), 'Y_umap'];
-plot.df = plot.df[cells_to_plot, ]
-
-gplot <- ggplot(plot.df, aes(x = umap1, y = umap2, color = zscore)) +
-    geom_point() +
-    scale_color_gradient(low = "#2c7bb6", high = "#d7191c") +
+gplot <- ggplot(plot_df, aes(x = lineage_step, y = score.zscore, fill = lineage_step)) +
+    geom_boxplot() +
+    # scale_fill_manual(
+    #     values = c(
+            
+    #         '1m' = '#fdc086',
+    #         '4m' = '#fc8d62',
+    #         '7m' = '#8da0cb',
+    #         'adult' = '#66c2a5'
+    #         # 'middle temporal gyrus' = '#e78ac3'
+    #         )
+    #     ) +
+    scale_x_discrete(labels = c(
+        'step1' = '2T RG-1',
+        'step2' = '2T RG-UL',
+        'step3' = '3T L4-RORB',
+        'step4' = '1M L4-PLCH1',
+        'step5' = '4M L4-PLCH1',
+        'step6' = '7M L4-PLCH1',
+        'step7' = 'Adult L4-PLCH1'
+        ))+
+    xlab('Lineage Step') +
+    ylab('met-scDRS') +
     theme_classic() +
-    ggtitle(gsub('_', ' ',disease)) +
-    theme(plot.title = element_text(hjust=0.5)) +
-    xlab(gsub('_', ' ', 'X_umap')) +
-    ylab(gsub('_', ' ', 'Y_umap')) +
-    theme(text = element_text(size = 20))
+    theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+    theme(text = element_text(size = 20)) +
+    theme(legend.position = "none")
 
-# draw out the plot:
-output.path <- paste0('/u/home/l/lixinzhe/project-geschwind/plot/', Sys.Date(), '-', disease, '-selected-scDRS-score-umap.png')
+output.path <- paste0('/u/home/l/lixinzhe/project-cluo/plot/', Sys.Date(), '-scz-lineage-step-box-plot.png')
 png(
     filename = output.path,
-    width = 14,
-    height = 14,
+    width = 15,
+    height = 10,
     units = 'in',
     res = 400
     );
 print(gplot)
 dev.off();
-
-
-
-# ### Do the Network diagram also ###################################################################
-# function.path <- '/u/home/l/lixinzhe/project-github/scDRS-applications/spell-book/'
-# source(paste0(function.path, 'score-loader.R'));
-# source(paste0(function.path, 'disease-score-expr-correlation.R'))
-# source(paste0(function.path, 'gene-ontology-caller.R'))
-# source(paste0(function.path, 'gs-decomposer.R'));
-
-# # load in the genes that went into the score:
-# gs.dir <- '/u/project/geschwind/lixinzhe/scDRS-output/magma-out/Kangcheng-gs/gs_file/';
-# gsea.c5 <- read.gmt('/u/project/geschwind/lixinzhe/data/c5.all.v2023.1.Hs.entrez.gmt');
-
-# trait.gs <- read.table(
-#     file = paste0(gs.dir, 'magma_10kb_top1000_zscore.75_traits.rv1.gs'),
-#     sep = '\t',
-#     header = TRUE
-#     );
-
-# # split gene sets:
-# trait.gene.set <- lapply(trait.gs$GENESET, gs.decomposer);
-# names(trait.gene.set) <- trait.gs$TRAIT;
-
-# ## subset to Schizophrenia disease:
-# disease = 'PASS_Schizophrenia_Pardinas2018'
-# frame = cbind(risk.score[[disease]], meta)
-
-# # load in the data:
-# ad <- anndata::read_h5ad('/u/project/cluo/lixinzhe/data/BICAN3/unnormalized_genebody_blacklist_allgenes_merged.h5ad')
-# expr <- as.matrix(ad$X)
-# rownames(expr) <- ad$obs_names
-# colnames(expr) <- ad$var_names
-
-# new_name = gsub('-0-0-0', '', rownames(expr))
-# new_name = gsub('-1-0$', '', new_name)
-# new_name = gsub('-1$', '', new_name)
-# new_name = gsub('-1-0-0$', '', new_name)
-
-# # get the common cells:
-# rownames(expr) = new_name
-# disease = 'PASS_Schizophrenia_Pardinas2018'
-# frame = cbind(risk.score[[disease]][new_name, ], meta)
-
-# # produce a vector of expr:
-# stopifnot(rownames(expr) == rownames(frame))
-# cor_vec = rep(NA, ncol(expr))
-# names(cor_vec) = colnames(expr)
-
-# # compute the gene expression to score correlation:
-# score.mch.cor <- expr.ds.cor(
-#     score = risk.score[disease],
-#     expression = expr,
-#     gene.set = trait.gene.set[disease]
-#     );
-
-# # compute the gene ontology enrichments:
-# ontology.gene.num = 100
-# top.genes = {}
-# top.genes[[disease]] <- names(head(sort(score.mch.cor[[disease]], decreasing = TRUE), ontology.gene.num));
-
-# bg.gene <- intersect(names(trait.gene.set[[disease]]), colnames(expr));
-
-# gene.ontology.result <- gene.ontology.caller(
-#     x = top.genes[[disease]],
-#     background = bg.gene,
-#     terms = gsea.c5,
-#     visualize = TRUE
-#     );
-# readable.result <- setReadable(gene.ontology.result, 'org.Hs.eg.db', 'ENTREZID')
-
-# # newwork plot:
-# cor.gene = head(sort(score.mch.cor[[disease]], decreasing = TRUE), ontology.gene.num)
-# network.plot <- cnetplot(
-#         readable.result,
-#         categorySize = "pvalue",
-#         color.params = list(foldChange = cor.gene, edge = TRUE),
-#         circular = TRUE) + 
-#         ggtitle(disease) +
-#         theme_classic() +
-#         theme(text = element_text(size = 20)) +
-#         theme(plot.title = element_text(hjust=0.5)) +
-#         theme(legend.text = element_text(size = 12))
-
-# network.path <- '/u/home/l/lixinzhe/project-geschwind/plot/'
-# plot.name <- paste0(network.path, Sys.Date(), '-SCZ-gene-ontology-enrichment-network-legend.png')
-# png(
-#     filename = plot.name,
-#     width = 10,
-#     height = 10,
-#     units = 'in',
-#     res = 400
-#     );
-
-# legend <- cowplot::get_legend(network.plot);
-# grid.newpage()
-# grid.draw(legend)
-# dev.off()
-
-# # plot out the network only:
-# plot.name <- paste0(network.path, Sys.Date(), '-SCZ-gene-ontology-enrichment-network.png')
-# network.plot <- network.plot + theme(legend.position = "none")
-# png(
-#     filename = plot.name,
-#     width = 10,
-#     height = 10,
-#     units = 'in',
-#     res = 400
-#     );
-# print(network.plot);
-# dev.off();
