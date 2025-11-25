8500af2f849b406d06e20a41014a5e08802073e9
Xinzhe Li <lixinzhe@ucla.edu>
2025-11-25 12:16:04 -0800

    compiled

--- code/met-scDRS/main-fig/potential_age_figure_E_I.R
+++ code/met-scDRS/main-fig/potential_age_figure_E_I.R
@@ -1,5 +1,5 @@
 ### processing script for GO is located at:
-'/u/home/l/lixinzhe/project-github/developmental-risk-scoring/code/met-scDRS/revised_run/gene-ontology-diseases.R'
+'/u/home/l/lixinzhe/project-github/developmental-risk-scoring/code/met-scDRS/revised_run/gene-ontology-age.R'
 
 
 # load in the work for generating network plot:
@@ -17,10 +17,14 @@ cor_collection = NULL
 go_collection = NULL
 
 traits.interest <- c('PASS_ADHD_Demontis2018', "PASS_Schizophrenia_Pardinas2018")
+combos = c('1m', '2T', '3T', '4-7m', 'adult')
 
-for (disease in traits.interest){
-    cor_collection[[disease]] = readRDS(paste0('/u/home/l/lixinzhe/project-geschwind/port/scratch/revision/GO/cov/', disease, '_score_mch_correlation.rds'))
-    go_collection[[disease]] = readRDS(paste0('/u/home/l/lixinzhe/project-geschwind/port/scratch/revision/GO/cov/', disease, '_readable_go_results.rds'))
+for (disease in c('SCZ', 'ADHD')){
+    for (combo in combos){
+        name = paste0(combo, '_', disease)
+        cor_collection[[name]] = readRDS(paste0('/u/home/l/lixinzhe/project-geschwind/port/scratch/revision/GO/age/', name, '_score_mcg_correlation.rds'))
+        go_collection[[name]] = readRDS(paste0('/u/home/l/lixinzhe/project-geschwind/port/scratch/revision/GO/age/', name, '_readable_go_results.rds'))
+    }
 }
 
 ###########################################################################################
@@ -29,23 +33,23 @@ for (disease in traits.interest){
 # save the gene ontology list:
 ontology.gene.num = 100
 
-for (disease in traits.interest){
-    readable.result = go_collection[[disease]]
-    cor.gene = head(sort(cor_collection[[disease]][[disease]], decreasing = TRUE), ontology.gene.num)
+for (combo in names(cor_collection)){
+    readable.result = go_collection[[combo]]
+    cor.gene = head(sort(cor_collection[[combo]], decreasing = TRUE), ontology.gene.num)
     network.plot <- cnetplot(
         readable.result,
         showCategory = 5,
         categorySize = "pvalue",
         color.params = list(foldChange = cor.gene, edge = TRUE),
         circular = TRUE) + 
-        ggtitle(disease) +
+        ggtitle(combo) +
         theme_classic() +
         theme(text = element_text(size = 25)) +
         theme(plot.title = element_text(hjust=0.5)) +
         theme(legend.text = element_text(size = 12))
 
     network.path <- '/u/home/l/lixinzhe/project-geschwind/plot/'
-    plot.name <- paste0(network.path, Sys.Date(), '-', disease, '-gene-ontology-enrichment-network-legend.png')
+    plot.name <- paste0(network.path, Sys.Date(), '-', combo, '-gene-ontology-enrichment-network-legend.png')
     png(
         filename = plot.name,
         width = 10,
@@ -59,7 +63,7 @@ for (disease in traits.interest){
     grid.draw(legend)
     dev.off()
         
-    plot.name <- paste0(network.path, Sys.Date(), '-', disease, '-gene-ontology-enrichment-network.png')
+    plot.name <- paste0(network.path, Sys.Date(), '-', combo, '-gene-ontology-enrichment-network.png')
     network.plot <- network.plot + theme(legend.position = "none")
     png(
         filename = plot.name,
